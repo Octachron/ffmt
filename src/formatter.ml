8fabdd3a5b95df3ec01a090d5dd9b18f55573935
octachron <octa@polychoron.fr>
2018-03-13 23:50:18 +0100

    Use Nlist GADT for storing open tags

--- src/formatter.ml
+++ src/formatter.ml
@@ -7,13 +7,15 @@ module Ft = Formatter_def
 module F = Format
 module Sem = Tagsem
 
+module NL = Ft.Nlist
+
 type open_tag = Open_tag : {tag: 'any D.tag; with_box: bool } -> open_tag
 
 type ('a,'b) t = ('a,'b) Ft.t
 
-let with_sem f ?(geometry=Geometry.default) ?(tags=[Sem.box]) x: ('a,F.z) t =
-  Ft.from_basic
-  { Ft.tag_semantic=tags; geometry; open_tags = []; metadata = E.start ( f x ) }
+let with_sem f ?(geometry=Geometry.default) ?(tags=[Sem.box]) x: ('a,Ft.z) t =
+  { Ft.tag_semantic=tags; geometry; open_tags = NL.[];
+    metadata = E.start ( f x ) }
 
 let chan ?geometry = with_sem (new Raw.chan) ?geometry
 let buffer ?geometry = with_sem (new Raw.buffer) ?geometry
@@ -29,89 +31,93 @@ exception No_open_tag
 type exn += Mismatched_close: {expected:'any Defs.tag; got:'other Defs.tag}
   -> exn
 
+let lift f s (ppf: _ t) =
+  { ppf with metadata = f ppf.geometry s ppf.metadata }
+
+let string x = lift E.string x
+let open_box x = lift E.open_box x
+let break x = lift E.break x
+let full_break x  = lift E.full_break x
+let close_box ppf = lift E.close_box () ppf
+let flush (ppf: _ t) = E.flush ppf.metadata
+
+let open_tag x (ppf: _ t) =
+  { ppf with open_tags = NL.( x :: ppf.open_tags ) }
 
 let rec eval:
   type free b right final before after.
-  <all:free; right:right; tail:b; fmt:final; tag_count:before -> after >
-    Format.format
+  <all:free; right:right; tail:b; fmt:final; tag_count:before -> after > F.format
   -> (free,right) Format.iargs
   -> (final,before) t -> (final,after) t  =
   let open Format in
   fun fmt iargs ppf -> match fmt with
     | [] -> ppf
-    | Literal s :: q  -> eval q iargs (Ft.string s ppf)
+    | Literal s :: q  -> eval q iargs (string s ppf)
     | Captured (k, f):: q ->
       let ppf = f iargs ppf in
       let iargs = Format.take k iargs in
       eval q iargs ppf
     | Open_tag (tag,data) :: q ->
-      begin match Sem.find_sem tag (Ft.semantics ppf) with
-        | None -> eval q iargs (Ft.open_tag ppf)
+      begin match Sem.find_sem tag ppf.tag_semantic with
+        | None -> eval q iargs (open_tag (Open_tag {with_box=false; tag}) ppf)
         | Some (sem, rest) ->
           let with_box, open_box =
             match sem#box tag data with
-            | None -> false, Ft.open_tag
-            | Some b -> true, Ft.open_box b in
-          let open_tags: _ list =
-            Sem.Open_tag { with_box; tag } :: (Ft.open_tags ppf) in
+            | None -> false, (fun ppf -> ppf)
+            | Some b -> true, open_box b in
+          let open_tags: _ NL.t =
+            Sem.Open_tag { with_box; tag } :: ppf.open_tags in
           let sem, p = sem#open_printer tag data in
           let tag_semantic: _ list = sem :: rest in
           let ppf = ppf |> open_box |> p in
-          eval q iargs (Ft.update  ppf ~open_tags ~tag_semantic )
+          eval q iargs { ppf  with open_tags; tag_semantic }
       end
     | Close_any_tag :: q ->
-      begin match Ft.open_tags ppf with
-        | [] -> raise No_open_tag
-        | Sem.Open_tag r :: tags -> close_tag r.with_box r.tag tags q iargs ppf
+      begin match ppf.open_tags with
+        | NL.(Sem.Open_tag r :: tags) ->
+          close_tag r.with_box r.tag tags q iargs ppf
       end
     | Close_tag ctag :: q ->
-      begin match Ft.open_tags ppf with
-        | [] -> raise No_open_tag
-        | Sem.Open_tag {tag; _ } :: _ when Name tag <> Name ctag ->
+      begin match ppf.open_tags with
+        | NL.( Sem.Open_tag {tag; _ } :: _ ) when Name tag <> Name ctag ->
           raise (Mismatched_close {expected=tag;got=ctag})
-        | Sem.Open_tag {tag; with_box} :: tags ->
+        | NL.( Sem.Open_tag {tag; with_box} :: tags ) ->
           close_tag with_box tag tags q iargs ppf
       end
     | Point_tag (tag, tdata) :: q ->
       let ppf =
-      begin match Sem.find_sem tag (Ft.semantics ppf) with
+      begin match Sem.find_sem tag ppf.tag_semantic with
         | None -> ppf
         | Some (sem,rest) ->
           let sem, p = sem#open_printer tag tdata in
           let ppf = p ppf in
           let ppf = begin match sem#break tag tdata with
             | None -> ppf
-            | Some Break {space; indent} ->
-              Ft.break {space;indent} ppf
-            | Some Full_break b ->
-              Ft.full_break b ppf
+            | Some Break {space; indent} -> break {space;indent} ppf
+            | Some Full_break b -> full_break b ppf
           end in
           let sem, p = sem#close_printer  in
           let ppf = p ppf in
-          Ft.update ~tag_semantic:(sem ::rest) ppf
+          { ppf with tag_semantic= sem ::rest }
       end in
       eval q iargs ppf
 
 and close_tag: type any n after free b right final.
-  bool -> any Defs.tag -> Sem.open_tag list
+  bool -> any Defs.tag -> (n, Sem.open_tag) NL.t
   -> <all:free; right:right; tail:b; fmt:final;
       tag_count: n -> after > Format.format
   -> (free,right) Format.iargs
   -> (final,n F.s) t -> (final, after) t =
   fun with_box tag open_tags q iargs ppf ->
-  match Sem.find_sem tag (Ft.semantics ppf) with
-  | None -> eval q iargs (Ft.close_box ppf)
+  match Sem.find_sem tag ppf.tag_semantic with
+  | None -> eval q iargs { ppf with open_tags }
   | Some (sem, rest) ->
     let sem, p =  sem#close_printer in
     (*  Printf.eprintf "Eval start %a\n%!" E.pp_pos ppf;*)
     let ppf = p ppf in
-    let ppf = if with_box then Ft.close_box ppf else Ft.close_tag ppf in
+    let ppf = if with_box then close_box ppf else ppf in
     (* Printf.eprintf "Eval %a\n%!" E.pp_pos ppf;*)
     let tag_semantic: _ list = sem :: rest in
-    eval q iargs (Ft.update ppf ~open_tags ~tag_semantic)
+    eval q iargs { ppf with open_tags; tag_semantic }
 
 let fprintf fmt args = eval fmt (Format.make args)
-
-let flush fmt = Ft.flush fmt
-let open_box, close_box, break, full_break, string = let open Ft in
-  open_box, close_box, break, full_break, string
--- src/formatter_def.ml
+++ src/formatter_def.ml
@@ -9,86 +9,28 @@ type 'a endo = 'a -> 'a
 type z = private Z
 type 'a s = private Succ
 
-module Inner: sig
+module Nlist = struct
+  type ('a,'b) t =
+    | [] : (z,'b) t
+    | (::): 'a * ('n,'a) t -> ('n s, 'a) t
+end
 
-  type (+'a,+'b) t
+type sem = <
+  mine:'any. 'any tag -> bool;
+  box: 'any. 'any tag -> 'any -> Defs.box option;
+  break: 'any. 'any tag -> 'any -> Defs.break option;
+  open_printer: 'any 'a 'b. 'any tag -> 'any ->
+    'self *
+    (('a,'b) t -> ('a,'b) t);
+  close_printer: 'a 'b. 'self * (('a,'b) t -> ('a,'b) t);
+> as 'self
 
-  type sem = <
-    mine:'any. 'any tag -> bool;
-    box: 'any. 'any tag -> 'any -> Defs.box option;
-    break: 'any. 'any tag -> 'any -> Defs.break option;
-    open_printer: 'any 'a 'b. 'any tag -> 'any ->
-      'self *
-      (('a,'b) t -> ('a,'b) t);
-    close_printer: 'a 'b. 'self * (('a,'b) t -> ('a,'b) t);
-  > as 'self
-
-  type 'a basic = {
-    geometry: Geometry.t;
-    tag_semantic: sem list;
-    open_tags: open_tag list;
-    metadata: 'a E.t
-  }
-
-  val open_tags: ('a,'b) t -> open_tag list
-  val semantics: ('a,'b) t -> sem list
-
-
-  val from_basic: 'a basic -> ('a,z) t
-  val to_basic: ('a,'b) t -> 'a basic
-  val string: string -> ('a,'b) t endo
-  val break: Defs.break_data -> ('a,'b) t endo
-  val full_break: int -> ('a,'b) t endo
-  val open_box: Defs.box -> ('a,'b) t -> ('a,'b s) t
-  val close_box: ('a,'b s) t -> ('a,'b) t
-  val open_tag: ('a,'b) t -> ('a,'b s) t
-  val close_tag: ('a,'b s) t -> ('a,'b) t
-  val update: ?open_tags:open_tag list -> tag_semantic:sem list
-    -> ('a,'b) t -> ('a,'b) t
-  val flush: ('a,z) t -> 'a
-end = struct
-  type (+'a,+'b) t = 'a basic
-  and sem = <
-    mine:'any. 'any tag -> bool;
-    box: 'any. 'any tag -> 'any -> Defs.box option;
-    break: 'any. 'any tag -> 'any -> Defs.break option;
-    open_printer: 'any 'a 'b. 'any tag -> 'any ->
-      'self *
-      (('a,'b) t -> ('a,'b) t);
-    close_printer: 'a 'b. 'self * (('a,'b) t -> ('a,'b) t);
-  > as 'self
-  and 'a basic = {
+and ('a,'n) t = {
   geometry: Geometry.t;
   tag_semantic: sem list;
-  open_tags: open_tag list;
+  open_tags: ('n, open_tag) Nlist.t;
   metadata: 'a E.t
 }
-
-  let from_basic x = x
-  let to_basic x = x
-
-  let semantics x = x.tag_semantic
-  let open_tags x = x.open_tags
-
-  let lift f s ppf =
-    { ppf with metadata = f ppf.geometry s ppf.metadata }
-
-  let string x = lift E.string x
-  let open_box x = lift E.open_box x
-  let open_tag x = x
-  let close_tag x = x
-  let break x = lift E.break x
-  let full_break x  = lift E.full_break x
-  let close_box ppf = lift E.close_box () ppf
-  let update ?open_tags ~tag_semantic ppf =
-    match open_tags with
-    | Some open_tags ->
-      { ppf with open_tags; tag_semantic }
-    | None -> { ppf with tag_semantic }
-  let flush ppf = E.flush ppf.metadata
-end
-include Inner
-
 
 class type semclass = object('self)
  method mine:'any. 'any tag -> bool
